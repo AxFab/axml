eff4360bac29f4cfb7214cf6d51802ba9500cab5
Fabien Bavent <fabien.bavent@gmail.com>
2014-10-28 01:48:43 +0100

    [+] Add BSD License

--- axml.js
+++ axml.js
@@ -1,8 +1,34 @@
 /*!
-*   Axml
-* Copyright 2014-2015 Fabien Bavent
-* Released under the BSD license
+    Axml
+   Copyright 2014-2015 Fabien Bavent
+  
+    ---------------------------------------------------------------------
+
+  All rights reserved.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+  * Neither the name of the author nor of its contributors may be used to 
+    endorse or promote products derived from this software without specific 
+    prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
 (function () {
   var previous_mod, root = this
   if (root != null)
@@ -137,6 +163,8 @@
     this.TEXT = opt.TEXT || axml.JSONML_TEXT
     this.ELEMENT = opt.ELEMENT || axml.JSONML_ELEMENT
     this.COMMENT = opt.COMMENT
+    this.DECLARATION = opt.DECLARATION
+    this.DOCTYPE = opt.DOCTYPE
   }
 
   // ======================================================================
@@ -250,16 +278,19 @@
 
     if (opt == null) {
       opt = {
-        depth:0, eol:'\n'
+        depth:0, eol:'\n', tabs:'  ',
       }
     } else {
       if (opt.depth == null) opt.depth = 0
       if (opt.eol == null) opt.eol = '\n'
+      if (opt.tabs == null) opt.tabs = '  '
     }
 
     opt.depthPadding = ''
-    for (var i=0; i<opt.depth; ++i)
-      opt.depthPadding += '  '
+    if (opt.tabs !== false) {
+      for (var i=0; i<opt.depth; ++i)
+        opt.depthPadding += tabs
+    }
     
     if (typeof data === 'string') {
       return opt.depthPadding + data + opt.eol
